996df52017cb33dedeeaa8178dcc4b2879e35d21
yanshan hu <yanshan hu>
2017-01-18 00:28:14 +0800

    change learning.py

--- learning.py
+++ learning.py
@@ -1,14 +1,19 @@
-class Dog():
+class Car():
 	"""docstring for Dog"""
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+	def __init__(self, make, model, year):
+		self.make = make
+		self.model = model
+		self.year = year
+		self.odometer = 0
 
-	def sit(self):
-		print(self.name.title() + 'is now sitting')
+	def get_descriptive_name(self):
+		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+
+	def read_odmeter(self):
+		print("This car has"self.model.title() + 'is now sitting')
 
 	def roll_over(self):
-		print(self.name.title() + "is rolling over")
+		print(self.model.title() + "is rolling over")
 		
 
 my_dog = Dog('jame', 3)
