377658b5f81658d6632e2d55ffa71713a268e65b
huyanshan <yanshan_huhu@qq.com>
2017-01-18 19:33:45 +0800

    Page 228

--- car.py
+++ car.py
@@ -21,3 +21,33 @@ class Car():
 			print("You can't roll back on odometer.")
 	def increment_odometer(self, miles):
 		self.odometer += miles
+
+
+class Battery():
+	"""docstring for Battery"""
+
+	def __init__(self, battery_size=70):
+		self.battery_size = battery_size
+
+	def describe_battery(self):
+		"""打印电瓶信息"""
+		print("This car has a " + str(self.battery_size) + "-kWh battery")
+
+	def get_range(self):
+		if self.battery_size == 70:
+			range = 240
+		elif self.battery_size == 85:
+			range = 270
+
+		message = "This car can go approximately " + str(range)
+		message += " miles on a full charge"
+		print(message)
+
+
+class ElectricCar(Car):
+	"""docstring for ElectricCar"""
+
+	def __init__(self, make, model, year):
+		''' 初始化父类属性 '''
+		super().__init__(make, model, year)
+		self.battery = Battery()
--- learning.py
+++ learning.py
@@ -21,38 +21,11 @@ class Car():
 	def increment_odometer(self, miles):
 		self.odometer += miles
 
-class Battery():
-	"""docstring for Battery"""
-	def __init__(self, battery_size=70):
-		self.battery_size = battery_size
 
-	def describe_battery(self):
-		"""打印电瓶信息"""
-		print("This car has a " + str(self.battery_size) + "-kWh battery")
-
-	def get_range(self):
-		if self.battery_size == 70:
-			range = 240
-		elif self.battery_size == 85:
-			range = 270
-		
-		message = "This car can go approximately " + str(range)
-		message += " miles on a full charge"
-		print(message)
-
-class ElectricCar(Car):
-	"""docstring for ElectricCar"""
-	def __init__(self, make, model, year):
-		''' 初始化父类属性 '''
-		super().__init__(make, model, year)
-		self.battery = Battery()
 
 	
 		
 
-my_tesla = ElectricCar('telsa', 'model_s', 2016)	
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+
 
 
